2b1c1caf953588cb254bce4c7f0bc82b8a36acdf
igorSerodio <igorserodioqueiroz@gmail.com>
2022-07-11 19:42:35 -0300

    commit inicial do projeto

---
+++ android/src/com/bb/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bb.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bb.game.BrainyBeans;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BrainyBeans(), config);
+	}
+}
